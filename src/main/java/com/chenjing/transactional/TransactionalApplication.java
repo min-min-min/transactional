6ebe52951e2c7329c3b5e0c887f520609a7f1d6e
chenjing <chenjing@gomo.com>
2020-09-17 11:53:02 +0800

    test

--- src/main/java/com/chenjing/transactional/TransactionalApplication.java
+++ src/main/java/com/chenjing/transactional/TransactionalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransactionalApplication {
 
+    //test
     public static void main(String[] args) {
         SpringApplication.run(TransactionalApplication.class, args);
     }
